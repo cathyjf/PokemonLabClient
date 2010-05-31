b08a59aa3e5e96c2219985dd26cf26ab24daa5ca
Carlos Fernandez <superstar90@gmail.com>
2010-05-31 13:52:45 -0400

    Made the Moves table pretty and shiny with (hopefully) proper mouseover events.

--- src/shoddybattleclient/utils/JButtonTable.java
+++ src/shoddybattleclient/utils/JButtonTable.java
@@ -7,8 +7,8 @@
  * I, Catherine Fitzpatrick, have made a trivial change by adding a subclass
  * of JTable for easier use of the code.
  */
-
 package shoddybattleclient.utils;
+
 import javax.swing.*;
 import javax.swing.table.*;
 import java.awt.event.*;
@@ -20,19 +20,20 @@ import java.awt.Component;
  * @author Catherine James Fitzpatrick
  */
 public class JButtonTable extends SortableJTable {
-    
+
     /**
      * Set up this table so that it can be used to render JButtons in cells.
      */
     public JButtonTable() {
         setDefaultRenderer(String.class, new RightAlignRenderer());
         setDefaultRenderer(JButton.class,
-                            new JTableButtonRenderer(
-                                getDefaultRenderer(JButton.class)));
-        addMouseListener(new JTableButtonMouseListener(this));
+                new JTableButtonRenderer(
+                getDefaultRenderer(JButton.class)));
+        JTableButtonMouseListener listener = new JTableButtonMouseListener(this);
+        addMouseListener(listener);
+        addMouseMotionListener(listener);
     }
 }
-
 
 /**
  * Renders components that are numerical or --- to be right aligned
@@ -49,89 +50,113 @@ class RightAlignRenderer implements TableCellRenderer {
         }
         return label;
     }
-
 }
+
 /**
  *
  * @author Daniel F. Savarese
  */
 class JTableButtonRenderer implements TableCellRenderer {
-  private TableCellRenderer __defaultRenderer;
 
-  public JTableButtonRenderer(TableCellRenderer renderer) {
-    __defaultRenderer = renderer;
-  }
+    private TableCellRenderer __defaultRenderer;
 
-  public Component getTableCellRendererComponent(JTable table, Object value,
-						 boolean isSelected,
-						 boolean hasFocus,
-						 int row, int column)
-  {
-    if(value instanceof Component)
-        return (Component)value;
-    return __defaultRenderer.getTableCellRendererComponent(
-	   table, value, isSelected, hasFocus, row, column);
-  }
+    public JTableButtonRenderer(TableCellRenderer renderer) {
+        __defaultRenderer = renderer;
+    }
+
+    public Component getTableCellRendererComponent(JTable table, Object value,
+            boolean isSelected, boolean hasFocus, int row, int column) {
+        if (value instanceof Component) {
+            return (Component) value;
+        }
+        return __defaultRenderer.getTableCellRendererComponent(
+                table, value, isSelected, hasFocus, row, column);
+    }
 }
-
-
 
 /**
  *
  * @author Daniel F. Savarese
  */
-class JTableButtonMouseListener implements MouseListener {
-  private JTable __table;
+class JTableButtonMouseListener implements MouseListener, MouseMotionListener {
 
-  private void __forwardEventToButton(MouseEvent e) {
-    TableColumnModel columnModel = __table.getColumnModel();
-    int column = columnModel.getColumnIndexAtX(e.getX());
-    int row    = e.getY() / __table.getRowHeight();
-    Object value;
-    JButton button;
-    MouseEvent buttonEvent;
+    private JTable __table;
+    private JButton previousButton;
 
-    if(row >= __table.getRowCount() || row < 0 ||
-       column >= __table.getColumnCount() || column < 0)
-      return;
+    private JButton __getButton(MouseEvent e) {
+        TableColumnModel columnModel = __table.getColumnModel();
+        int column = columnModel.getColumnIndexAtX(e.getX());
+        int row = e.getY() / __table.getRowHeight();
+        Object value;
 
-    value = __table.getValueAt(row, column);
+        if (row >= __table.getRowCount() || row < 0
+                || column >= __table.getColumnCount() || column < 0) {
+            return null;
+        }
 
-    if(!(value instanceof JButton))
-      return;
+        value = __table.getValueAt(row, column);
 
-    button = (JButton)value;
+        if (!(value instanceof JButton))
+            return null;
 
-    buttonEvent =
-      (MouseEvent)SwingUtilities.convertMouseEvent(__table, e, button);
-    button.dispatchEvent(buttonEvent);
-    // This is necessary so that when a button is pressed and released
-    // it gets rendered properly.  Otherwise, the button may still appear
-    // pressed down when it has been released.
-    __table.repaint();
-  }
+        return (JButton) value;
+    }
 
-  public JTableButtonMouseListener(JTable table) {
-    __table = table;
-  }
+    public JTableButtonMouseListener(JTable table) {
+        __table = table;
+    }
 
-  public void mouseClicked(MouseEvent e) {
-    __forwardEventToButton(e);
-  }
+    public void mouseClicked(MouseEvent e) {
+        JButton button = __getButton(e);
 
-  public void mouseEntered(MouseEvent e) {
-    __forwardEventToButton(e);
-  }
+        if(button == null)
+            return;
 
-  public void mouseExited(MouseEvent e) {
-    __forwardEventToButton(e);
-  }
+        MouseEvent buttonEvent =
+                    (MouseEvent) SwingUtilities.convertMouseEvent(__table, e, button);
+        button.dispatchEvent(buttonEvent);
+        __table.repaint();
+    }
 
-  public void mousePressed(MouseEvent e) {
-    __forwardEventToButton(e);
-  }
+    public void mouseEntered(MouseEvent e) {}
 
-  public void mouseReleased(MouseEvent e) {
-    __forwardEventToButton(e);
-  }
+    public void mouseExited(MouseEvent e) {
+        if(previousButton != null) {
+            previousButton.dispatchEvent(new MouseEvent(previousButton, MouseEvent.MOUSE_EXITED,
+                        e.getWhen(), e.getModifiers(), 5, 5, 0, false));
+        }
+
+        previousButton = null;
+        __table.repaint();
+    }
+
+    public void mousePressed(MouseEvent e) {
+        mouseClicked(e);
+    }
+
+    public void mouseReleased(MouseEvent e) {
+        mouseClicked(e);
+    }
+
+    public void mouseDragged(MouseEvent e) {}
+
+    public void mouseMoved(MouseEvent e) {
+        JButton button = __getButton(e);
+
+        if(button == previousButton)
+            return;
+
+        if(previousButton != null) {
+            previousButton.dispatchEvent(new MouseEvent(previousButton, MouseEvent.MOUSE_EXITED,
+                    e.getWhen(), e.getModifiers(), 5, 5, 0, false));
+        }
+
+        if (button != null) {
+            button.dispatchEvent(new MouseEvent(button, MouseEvent.MOUSE_ENTERED,
+                    e.getWhen(), e.getModifiers(), 5, 5, 0, false));
+        }
+        
+        __table.repaint();
+        previousButton = button;
+    }
 }
